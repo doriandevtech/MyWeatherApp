24e4daa30ed70d239d1f9ed00de999833c04b119
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-07-15 16:28:41 +0200

    add basic components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+
+function App() {
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  };
+
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,41 @@
+import "./current-weather.css";
+
+const CurrentWeather = () => {
+  return (
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">Belgrade</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
+      </div>
+      <div className="bottom">
+        <p className="temperature">18°C</p>
+        <p className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">22°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">10 hPa</span>
+          </div>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CurrentWeather;
