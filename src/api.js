499a940f90b3c5abd2ea174928f3ba42503ec6a3
Dorian <dorianemenir@gmail.com>
2022-07-16 20:55:20 +0200

    Delete api.js

--- src/api.js
+++
@@ -1,9 +0,0 @@
-export const geoApiOptions = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "ca5ee049b3msh16d8d15ec096abep1a998fjsnc9802ac0409d",
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-  },
-};
-
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
